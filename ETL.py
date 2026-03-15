5089c589526d5cc862bbe12da1642b5b563258d7
Fhamamep <fhamamep@udd.cl>
2026-03-15 16:52:28 +0100

    Sprint 1

--- ETL.py
+++ ETL.py
@@ -9,63 +9,55 @@ import pandas as pd
 
 # -------EXTRACT----
 
-def get_dow_reverse_history():
-    # Final List (Current State for 2026/2025)
-    # This reflects the November 8, 2024 changes (NVDA & SHW added)
-    current_dow = [
-        "MMM", "AXP", "AMGN", "AMZN", "AAPL", "BA", "CAT", "CVX", "CSCO", "KO",
-        "DIS", "GS", "HD", "HON", "IBM", "JNJ", "JPM", "MCD", "MRK", "MSFT",
-        "NKE", "NVDA", "PG", "CRM", "SHW", "TRV", "UNH", "VZ", "V", "WMT"
-    ]
+# Dow 30 composition in 2003
+dow_2003 = [
+    "MMM", "AA", "MO", "AXP", "T",
+    "BA", "CAT", "C", "KO", "DD", 
+    "EK", "XOM", "GE", "GM", "HPQ", 
+    "HD", "HON", "INTC", "IBM", "IP", 
+    "JNJ", "JPM", "MCD", "MRK", "MSFT", 
+    "PG", "SBC", "UTX", "WMT", "DIS"
+]
 
-    history = {}
-    
-    # We work backward from 2026
-    # To go back in time, 'reverse_add' means we remove it, 
-    # and 'reverse_remove' means we put the old company back.
-    backwards_steps = {
-        2024: {"reverse_add": ["NVDA", "SHW", "AMZN"], "reverse_remove": ["INTC", "DOW", "WBA"]},
-        2020: {"reverse_add": ["AMGN", "HON", "CRM"], "reverse_remove": ["XOM", "PFE", "RTX"]},
-        2019: {"reverse_add": ["DOW"], "reverse_remove": ["DowDuPont"]},
-        2018: {"reverse_add": ["WBA"], "reverse_remove": ["GE"]},
-        2017: {"reverse_add": ["DowDuPont"], "reverse_remove": ["DD"]},
-        2015: {"reverse_add": ["AAPL"], "reverse_remove": ["T"]},
-        2013: {"reverse_add": ["GS", "NKE", "V"], "reverse_remove": ["AA", "BAC", "HPQ"]},
-        2012: {"reverse_add": ["UNH"], "reverse_remove": ["KFT"]},
-        2009: {"reverse_add": ["TRV", "CSCO"], "reverse_remove": ["GM", "C"]},
-        2008: {"reverse_add": ["KFT", "CVX", "BAC"], "reverse_remove": ["AIG", "MO", "HON"]},
-        2004: {"reverse_add": ["AIG", "PFE", "VZ"], "reverse_remove": ["T", "EK", "IP"]},
+def get_dow_forward_history():
+    # Historical changes from 2003 to present
+    forward_steps = {
+        2004: {"remove": ["T", "EK", "IP"], "add": ["AIG", "PFE", "VZ"]},
+        2005: {"remove": ["SBC"], "add": ["T"]},
+        2008: {"remove": ["AIG", "MO", "HON"], "add": ["KFT", "CVX", "BAC"]},
+        2009: {"remove": ["GM", "C"], "add": ["TRV", "CSCO"]},
+        2012: {"remove": ["KFT"], "add": ["UNH"]},
+        2013: {"remove": ["AA", "BAC", "HPQ"], "add": ["GS", "NKE", "V"]},
+        2015: {"remove": ["T"], "add": ["AAPL"]},
+        2017: {"remove": ["DD"], "add": ["DWDP"]},
+        2018: {"remove": ["GE"], "add": ["WBA"]},
+        2019: {"remove": ["DWDP"], "add": ["DOW"]},
+        2020: {"remove": ["XOM", "PFE", "UTX"], "add": ["AMGN", "HON", "CRM"]},
+        2024: {"remove": ["INTC", "DOW", "WBA"], "add": ["NVDA", "SHW", "AMZN"]},
     }
 
-    temp_list = list(current_dow)
+    history = {}
+    temp_list = list(dow_2003)
+    history[2003] = sorted(list(temp_list))
     
-    for year in range(2026, 1999, -1):
-        if year in backwards_steps:
-            # Reversing the change: remove what was added, re-add what was removed
-            for ticker in backwards_steps[year]["reverse_add"]:
-                if ticker in temp_list: temp_list.remove(ticker)
-            for ticker in backwards_steps[year]["reverse_remove"]:
+    for year in range(2004, 2027):
+        if year in forward_steps:
+            for ticker in forward_steps[year]["remove"]:
+                if ticker in temp_list:
+                    temp_list.remove(ticker)
+            for ticker in forward_steps[year]["add"]:
                 temp_list.append(ticker)
         
         history[year] = sorted(list(temp_list))
-        
+    
     return history
 
+
 # Generate data
-dow_history = get_dow_reverse_history()
-
-
-#  for example, accessing the year 2005
-dow_history_2005= dow_history[2005]
-print(f"Dow 30 in 2005: {dow_history_2005}")
+dow_history = get_dow_forward_history()
 
 
 
-# Download DOW data for 10 years
-tickers= dow_history_2005
-dow = yf.download(tickers, period="10y")
-
-print(f"DOW Data from 2005 tickets:{dow.head()}")
 
 
 
---
+++ Main.py
@@ -0,0 +1,11 @@
+
+# OPTIMIZATION IMPLEMENATION
+
+
+# MVO
+
+
+
+
+
+# 
